389e263761b0da044fa6470d29765edc32788848
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-07-29 19:49:58 +0800

    init.rc: symlink /lib to /system/lib
    Currently more linux drivers use request_firmware_nowait() to load
    firmwares. It requires the firmwares exist in the standard path.
    
    Create the symlink before ueventd started to ensure firmwares
    could be loaded correctly.
    
    
    Former-commit-id: d70c5b9d682f743f01fd6f5d757412b2be883d9b
    Former-commit-id: f9c58b40c42574b097537082fb145910b019c157

--- rootdir/create_root_structure.mk
+++ rootdir/create_root_structure.mk
@@ -30,6 +30,7 @@ LOCAL_POST_INSTALL_CMD := mkdir -p $(addprefix $(TARGET_ROOT_OUT)/, \
     dev proc sys system data data_mirror odm oem config storage mnt apex bootstrap-apex debug_ramdisk \
     linkerconfig second_stage_resources postinstall tmp $(BOARD_ROOT_EXTRA_FOLDERS)); \
     ln -sf /system/bin $(TARGET_ROOT_OUT)/bin; \
+    ln -sf /system/lib $(TARGET_ROOT_OUT)/lib; \
     ln -sf /system/etc $(TARGET_ROOT_OUT)/etc; \
     ln -sfn /sys/kernel/debug $(TARGET_ROOT_OUT)/d; \
     ln -sf /storage/self/primary $(TARGET_ROOT_OUT)/sdcard; \
